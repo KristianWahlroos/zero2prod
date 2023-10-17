2be3822f1284e7e8f0e0093593ea6703ecb631ad
kristianwahlroos <kristian.whl@gmail.com>
2023-10-17 15:45:40 +0300

    refactor: split lib into main and lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 use actix_web::*;
 
-#[tokio::main]
-async fn main() -> std::io::Result<()> {
+pub async fn run() -> std::io::Result<()> {
     HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
         .bind("127.0.0.1:8000")?
         .run()
